9a60bdfeb9ae5fd954ec06a255262819a41358c6
Dare Lawal <lawaldare@gmail.com>
2020-08-12 14:22:44 +0100

    navbar component implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {ComponentsModule} from './components/components.module';
+import { ComponentsModule } from './components/components.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EventsComponent } from './components/events/events.component';
+import { PhotosComponent } from './components/photos/photos.component';
+import { MembersComponent } from './components/members/members.component';
+import { JobsComponent } from './components/jobs/jobs.component';
+import { NavBarGeneralComponent } from './components/nav-bar-general/nav-bar-general.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    EventsComponent,
+    PhotosComponent,
+    MembersComponent,
+    JobsComponent,
+    NavBarGeneralComponent,
+    NavBarComponent,
+
   ],
   imports: [
     BrowserModule,
