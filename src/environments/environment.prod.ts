ed03bc15e3915838261934bb293494b65a821581
Kelvin Esekhile <kelvinoesekhile@gmail.com>
2021-03-08 19:47:03 +0100

    update key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  token: 'sk5OnvEtAnxR8QfFwoA66w4Ze8NW2CtGlf3VrOwzpiAtzIoy5CHvCuQh7JUaXNzC2us0jSe3L2eurZHel4VxmNmFvjiWdG0qwav89YI0k2L7kqCxTuG6QAf0s7JKWukiClEtWz2b3pHt1WoVi1BKpQKlSlocrYWx2aEKJQUhFvoFhejRhFnP`',
+  token: 'sk5OnvEtAnxR8QfFwoA66w4Ze8NW2CtGlf3VrOwzpiAtzIoy5CHvCuQh7JUaXNzC2us0jSe3L2eurZHel4VxmNmFvjiWdG0qwav89YI0k2L7kqCxTuG6QAf0s7JKWukiClEtWz2b3pHt1WoVi1BKpQKlSlocrYWx2aEKJQUhFvoFhejRhFnP',
   dataSet: 'production'
 };
